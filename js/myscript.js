352011122622ad682953d3897666c7957bb37426
Lezd <lilinouxgaming@gmail.com>
2020-04-18 13:13:34 +0200

    Fix des subs à offrir si on utilise l'aide.

--- js/myscript.js
+++ js/myscript.js
@@ -53,12 +53,12 @@ function aide(){
 function add(type){
     switch (type){
         case 'noix':
-            if(app.noix!==900 && app.stateQuestNoix !== "☑"){app.noix++; app.help?app.subToPay+=20:{}}
+            if(app.noix!==900 && app.stateQuestNoix !== "☑"){app.noix++; app.help?app.subToPay+=5:{}}
             if(app.noix===900 && app.stateQuestNoix !== "☑"){ randomizer(20);  app.stateQuestNoix = "☑"; app.CompleteOneOf=true; eastereggBG('dance', 8000);}
             checkend();
             break;
         case 'sanctu':
-            if(app.sanctu!==120 && app.stateQuestSanctu !== "☑"){app.sanctu++; app.help?app.subToPay+=20:{}}
+            if(app.sanctu!==120 && app.stateQuestSanctu !== "☑"){app.sanctu++; app.help?app.subToPay+=15:{}}
             if(app.sanctu===120 && app.stateQuestSanctu !== "☑"){ randomizer(200);  app.stateQuestSanctu = "☑"; app.CompleteOneOf=true; eastereggBG('dance', 8000);}
             checkend();
             break;
@@ -71,7 +71,7 @@ function add(type){
             checkend();
             break;
         case 'hinox':
-            if(app.hinox!==40 && app.stateQuestHinox !== "☑"){app.hinox++; app.help?app.subToPay+=20:{}}
+            if(app.hinox!==40 && app.stateQuestHinox !== "☑"){app.hinox++; app.help?app.subToPay+=10:{}}
             if(app.hinox===40 && app.stateQuestHinox !== "☑"){ randomizer(20);  app.stateQuestHinox = "☑"; app.CompleteOneOf=true; eastereggBG('fin', 4000);}
             checkend();
             break;
